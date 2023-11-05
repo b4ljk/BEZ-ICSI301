188610ab1d56492aa350bf94acaf91759d920f9d
Jessica <91076549+Sumikose@users.noreply.github.com>
2023-11-05 22:58:58 +0800

    Update apicaller.js

--- javascript/apicaller.js
+++ javascript/apicaller.js
@@ -2,7 +2,8 @@ console.log("apicaller.js loaded");
 // fetch data at localhost:3000
 let result = "";
 async function data() {
-	await fetch("http://127.0.0.1:3000/")
+	await fetch("https://d9tgfokrrl.execute-api.eu-north-1.amazonaws.com/prod/user")
+	// await fetch("http://127.0.0.1:3000/")
 		.then((response) => response.json())
 		.then((data) =>
 			data?.map((item) => {
